ae8c7a90dbbc168997131023b02d7633fa70ef23
Abizer Nasir <abizern@abizern.org>
2014-01-09 16:51:35 +0000

    Move deploy script into hblog.hs
    No point in having a separate script for it.

--- hblog.hs
+++ hblog.hs
@@ -214,5 +214,27 @@ feedConfiguration title = FeedConfiguration
                           , feedRoot        = "http://abizern.org"
                           }
 
+--------------------------------------------------------------------------------
+-- | Deployment
 config :: Configuration
-config = defaultConfiguration { deployCommand = "./_publish.sh" }
+config = defaultConfiguration { deployCommand = deployScript }
+
+deployScript :: String
+deployScript = "echo \"-- Publishing to abizern.org\" &&\
+               \echo \"-- Rebuilding site...\" && \
+               \./hblog rebuild && \
+               \cd _publish && \
+               \echo \"-- Updating deploy repository...\" && \
+               \git fetch github && \
+               \git reset --hard github/master && \
+               \echo \"-- Adding new files to deploy repository...\" && \
+               \rm -rf * && \
+               \cp -r ../_site/ . && \
+               \echo \"-- Commiting changes\" && \
+               \dt=`date \"+%Y-%m-%d %H:%M:%S %Z\"` && \
+               \message=\"Site update at $dt\" && \
+               \git add . && \
+               \git commit -m\"$message\" && \
+               \echo \"-- Deploying site\" && \
+               \git push github master && \
+               \echo \"-- Site published\""
